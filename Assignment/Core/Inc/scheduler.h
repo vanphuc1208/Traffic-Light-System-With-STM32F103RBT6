e5e5271b74a51f12c5005b4078ec0d77e66812ea
vanphuc1208 <levanphucdn2003@gmail.com>
2023-11-27 09:48:35 +0700

    scheduler first

---
+++ Assignment/Core/Inc/scheduler.h
@@ -0,0 +1,18 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: Nov 25, 2023
+ *      Author: OS
+ */
+
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
+#include"main.h"
+
+
+#endif /* INC_SCHEDULER_H_ */
+ void SCH_Init(void);
+ void SCH_Update(void);
+ void SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
+ void SCH_Dispatch_Tasks(void);
+ void SCH_Delete_Task(uint32_t TASK_ID);
