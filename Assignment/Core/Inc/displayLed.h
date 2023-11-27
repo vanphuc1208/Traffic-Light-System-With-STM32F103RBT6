f19507eb05b214d593f351e3ebf0bde42aa765ae
vanphuc1208 <levanphucdn2003@gmail.com>
2023-11-27 10:32:59 +0700

    done controlLed

---
+++ Assignment/Core/Inc/displayLed.h
@@ -0,0 +1,25 @@
+/*
+ * displayLed.h
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: OS
+ */
+#ifndef INC_DISPLAYLED_H_
+#define INC_DISPLAYLED_H_
+
+void clearAllLed1();
+void clearAllLed2();
+void displayRed1();
+void displayYellow1();
+void displayGreen1();
+void displayRed2();
+void displayYellow2();
+void displayGreen2();
+void toggleRed();
+void togglYellow();
+void toggleGreen();
+void clearPedestrian();
+void displayPedestrianRed();
+void displayPedestrianYellow();
+void displayPedestrianGreen();
+#endif /* INC_DISPLAYLED_H_ */
--- Assignment/Debug/Core/Src/subdir.mk
+++ Assignment/Debug/Core/Src/subdir.mk
@@ -5,6 +5,8 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/displayLed.c \
+../Core/Src/fsm.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
@@ -13,6 +15,8 @@ C_SRCS += \
 ../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
+./Core/Src/displayLed.o \
+./Core/Src/fsm.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
@@ -21,6 +25,8 @@ OBJS += \
 ./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
+./Core/Src/displayLed.d \
+./Core/Src/fsm.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
