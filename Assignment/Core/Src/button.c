d4a1cf36c7438da5f99a0233cd25421bc289a1c7
LAPTOP-HH2P921E\thien <nguyenquangthien1506@gmail.com>
2023-11-30 01:41:39 +0700

    INIT2

--- Assignment/Core/Src/button.c
+++ Assignment/Core/Src/button.c
@@ -7,26 +7,38 @@
 
 #include "button.h"
 #include "main.h"
-//we aim to work with more than one buttons
+
+// Aim to work with more than one buttons
+
 #define N0_OF_BUTTONS 				       4
-//timer interrupt duration is 10ms, so to pass 1 second,
-//we need to jump to the interrupt service routine 100 time
+
+// Timer interrupt duration is 10ms, so to pass 1 second,
+// Need to jump to the interrupt service routine 100 time
+
 #define DURATION_FOR_AUTO_INCREASING	   100
 #define BUTTON_IS_PRESSED                  GPIO_PIN_RESET
 #define BUTTON_IS_RELEASED                 GPIO_PIN_SET
-//the buffer that the final result is stored after
-//debouncing
+
+// The buffer that the final result is stored after
+// Debouncing
 static GPIO_PinState buttonBuffer[N0_OF_BUTTONS]={BUTTON_IS_RELEASED,BUTTON_IS_RELEASED,BUTTON_IS_RELEASED};
+
 GPIO_TypeDef* buttonGPIO[N0_OF_BUTTONS]= {A0_GPIO_Port,A1_GPIO_Port,A2_GPIO_Port,A3_GPIO_Port };
+
 static int buttonPin[N0_OF_BUTTONS]={A0_Pin, A1_Pin, A2_Pin, A3_Pin};
+
 int buttonTimeOut[N0_OF_BUTTONS] = {0};
-//we define two buffers for debouncing
+
+// Defining two buffers for debouncing
+
 static GPIO_PinState debounceButtonBuffer1[N0_OF_BUTTONS];
 static GPIO_PinState debounceButtonBuffer2[N0_OF_BUTTONS];
 static GPIO_PinState debounceButtonBuffer3[N0_OF_BUTTONS];
-//we define a flag for a button pressed more than 1 second.
-//we define counter for automatically increasing the value
-//after the button is pressed more than 1 second.
+
+// Defining  a flag for a button pressed more than 1 second.
+// Defining  counter for automatically increasing the value
+// After the button is pressed more than 1 second.
+
 int buttonFlag[N0_OF_BUTTONS] = {0};
 void button_reading(void)
 {
@@ -58,7 +70,8 @@ void button_reading(void)
 
 int is_button_pressed(int index)
 {
-	if(index >= N0_OF_BUTTONS || index < 0) return 0;
+	if(index >= N0_OF_BUTTONS || index < 0)	return 0;
+
 		if(buttonFlag[index] == 1)
 		{
 			buttonFlag[index] = 0;
